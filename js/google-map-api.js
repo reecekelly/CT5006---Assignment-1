9b897b31f09db7a5c0b2fe1e33b8a78264291d3a
reecekelly <xXreecekellyXx@gmail.com>
2015-01-01 23:32:06 +0000

    Pretty sure it's fixed now
    Using old files to hopefully repair the appcache

--- js/google-map-api.js
+++ js/google-map-api.js
@@ -1,28 +1,24 @@
-			if(navigator.geolocation) {
-				
-				function hasPosition(position) {
-					var point = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-					var universty = new google.maps.LatLng(51.886708, -2.088914),
+	if(navigator.geolocation) {
+					
+		function hasPosition(position) {
+			var point = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
 						
-					myOptions = {
-						zoom: 15,
-						center: point,
-						mapTypeId: google.maps.MapTypeId.ROADMAP
-					}, 
-					mapDiv = document.getElementById("mapDiv"),
+			myOptions = {
+				zoom: 15,
+				center: point,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			}, 
+						
+			mapDiv = document.getElementById("mapDiv"),
 					
-					map = new google.maps.Map(mapDiv, myOptions),
-						marker = new google.maps.Marker({
-						position: point,
-						map: map,
-						title: "You are here"
-					});
-					
-					var marker = new google.maps.Marker({
-						position: universty,
-						map: map,
-						title: "Park Campus"
-					});
-				}
-				navigator.geolocation.getCurrentPosition(hasPosition);
-			}
+			map = new google.maps.Map(mapDiv, myOptions),
+				marker = new google.maps.Marker({
+				position: point,
+				map: map,
+				title: "You are here"
+			});
+		}
+			
+		navigator.geolocation.getCurrentPosition(hasPosition);
+		google.maps.event.addDomListener(window, 'load', initialize);
+	}
